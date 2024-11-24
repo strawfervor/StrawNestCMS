9e959a520c167dd62ee389f5212f7c8caf51bdd0
strawfervor <strawfervor@gmail.com>
2024-11-24 10:49:43 +0000

    added preview_len and items varibles

--- config.php
+++ config.php
@@ -1,3 +1,8 @@
 title: StrawNestCMS
 heading1: StrawNestCMS changelog
-heading2: What a wonderfull cms :D :D :D 
+heading2: What a wonderfull cms :D :D :D
+preview_len: 300
+items: 5
+
+//preview_len - lenght of entry preview
+//items - number of items shown on each page
--- index.php
+++ index.php
@@ -4,19 +4,41 @@
     $title = "";
     $heading1 = "";
     $heading2 = "";
+    $preview_len = 300;
+    $items = 5;
+
 //loop for reading varibles from config file
     while (!feof($config)) {
         $config_line = fgets($config);
-        if (strpos($config_line, "title:") !== false || strpos($config_line, "title") === 0) {
+        if (match_config_line($config_line, "title:")) {
             global $title; 
             $title = substr($config_line, 6);
-        } elseif (strpos($config_line, "heading1:") !== false || strpos($config_line, "heading1") === 0) {
+        } elseif (match_config_line($config_line, "heading1:")) {
             global $heading1; 
             $heading1 = substr($config_line, 9);
-        } elseif (strpos($config_line, "heading2:") !== false || strpos($config_line, "heading2") === 0) {
+        } elseif (match_config_line($config_line, "heading2:")) {
             global $heading2; 
             $heading2 = substr($config_line, 9);
+        } elseif (match_config_line($config_line, "preview_len:")) {
+            global $preview_len;
+            $temp = intval(substr($config_line, 12));
+            if ($temp >= 0 && $temp !== $preview_len) {
+                $preview_len = $temp;
+            }
+        } elseif (match_config_line($config_line, "items:")) {
+            global $items;
+            $temp = intval(substr($config_line, 6));
+            if ($temp >= 1 && $temp !== $items && $temp <= 1000) {
+                $items = $temp;
+            }
         }
+    }
+
+    function match_config_line($config_line, $config_parameter) {
+        if (strpos($config_line, $config_parameter) !== false || strpos($config_line, $config_parameter) === 0) {
+            return true;
+        }
+        return false;
     }
 ?>
 
